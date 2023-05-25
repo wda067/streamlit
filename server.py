5ee14c7abe71efe5450ba59bd2d601b344d16e66
wda067 <75603567+wda067@users.noreply.github.com>
2023-05-25 17:47:48 +0900

    Update server.py

--- server.py
+++ server.py
@@ -43,9 +43,11 @@ def main():
         if lp:            
             st.write('번호판 인식 결과 : ', lp)
             if st.button("파일명 저장", key=file_name):
-                with open(os.path.join(f.name.split('tmp')[0], file.name.split('.jpg')[0] + ' ' + lp + '.jpg'), "wb") as f1:
-                    f1.write(file.getbuffer())
-                st.write("파일 저장 위치 : ",f1.name)
+#                 with open(os.path.join(f.name.split('tmp')[0], file.name.split('.jpg')[0] + ' ' + lp + '.jpg'), "wb") as f1:
+#                     f1.write(file.getbuffer())
+#                 st.write("파일 저장 위치 : ",f1.name)
+                with open(f.name, 'rb') as f2:
+                    st.download_button(label="파일 다운로드", data=file, file_name=file.name.split('.jpg')[0] + ' ' + lp + '.jpg',mime="image/jpg")
                 # img2 = cv2.imread(file_name)
                 # os.rename(file_name, file_name.split('.jpg')[0] + ' ' +''.join(lp) + '.jpg')
         # if st.button("번호판 변경하기", key=time):
